03dd5ae20388195b9348300a5dc3d93b0d9ddceb
rosemaryellery <98529835+rosemaryellery@users.noreply.github.com>
2022-09-02 14:17:46 +0100

    Radio button functionality for plotting added.

--- main/views.py
+++ main/views.py
@@ -1,10 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import PerceptronModel
+# from .models import PerceptronModel
 from .utils import get_plot
 # Create your views here.
 
-def index(response):
-    qs = PerceptronModel()
-    chart = get_plot(qs.x_values, qs.y_values, qs.distances_matrix)
-    return render(response, 'main/index.html', {'chart':chart})
+def index(request):
+    gate_type = 'AND'
+    if request.method == 'POST':
+        gate_type = request.POST.get('gate')
+    labels = [0, 0, 0, 1]
+    if gate_type == 'AND':
+        labels = [0, 0, 0, 1]
+        desc = 'As shown in the above plot, the perceptron can plot a clear decision boundary for the AND gate as it linearly-seperable.'
+    elif gate_type == 'OR':
+        labels = [0, 1, 1, 1]
+        desc = 'When trained on an OR gate, the perceptron displays a linear decision boundary.'
+    elif gate_type == 'XOR':
+        labels = [0, 1, 1, 0]
+        desc = 'The possible outputs of an XOR gate are not linearly-seperable, therefore the plot shows no decision boundary. This demonstrates the limitations of using a perceptron model.'
+    x_values, y_values, distances_matrix = PerceptronModel(labels)
+    chart = get_plot(x_values, y_values, distances_matrix)
+    return render(request, 'main/index.html', {'chart':chart, 'gate_type': gate_type, 'desc':desc})
+    
+
+def PerceptronModel(labels):
+    # import codecademylib3_seaborn
+    from sklearn.linear_model import Perceptron
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from itertools import product
+
+    data = [[0, 0], [0, 1], [1, 0], [1, 1]]
+
+    # plt.scatter([point[0] for point in data], [point[1] for point in data], c=labels)
+    # plt.show()
+
+    classifier = Perceptron(max_iter = 40)
+    classifier.fit(data, labels)
+    # print(classifier.score(data, labels))
+
+    # print(classifier.decision_function([[0, 0], [1, 1], [0.5, 0.5]]))
+
+    x_values = np.linspace(0, 1, 100)
+    y_values = np.linspace(0, 1, 100)
+
+
+    point_grid = list(product(x_values, y_values))
+
+    distances = classifier.decision_function(point_grid)
+
+    abs_distances = [abs(point) for point in distances]
+
+    distances_matrix = np.reshape(abs_distances, (100, 100))
+    
+    return x_values, y_values, distances_matrix
